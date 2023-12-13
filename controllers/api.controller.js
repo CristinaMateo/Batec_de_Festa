045d06f7d1dd113ef14e51c9e485245dad99e1fd
Cristina Mateo <cris.mateo.99@gmail.com>
2023-12-13 18:55:36 +0100

    get and post routes

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,1 +1,30 @@
 const api = require ('../models/api.model')
+
+const getAllEvents = async (req, res) => {
+    let events;
+    
+        events = await api.getAllEvents();//esto accede a entries.models y llama a esa funcion allí
+    
+    res.status(200).json(events); // [] con las entries encontradas
+}
+
+const getMyEvents = async (req, res) => {
+    let events;
+    events = await api.getMyEvents(req.query.email)
+    res.status(200).json(events); 
+}
+
+const createEvent = async (req, res) => {
+    const newEvent = req.body; // {title,content,email,category}
+    const response = await api.createEvent(newEvent);//esto accede a entries.models y llama a esa funcion allí
+    res.status(201).json({
+        "items_created": response,
+        data: newEvent
+    });
+}
+
+module.exports ={
+    getAllEvents,
+    getMyEvents,
+    createEvent
+}
--- routes/api.routes.js
+++ routes/api.routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const apiController = require("../controllers/api.controller");
 const apiRouter = express.Router();
 
-apiRouter.get()//todos los eventos de la bbdd
-apiRouter.get()//eventos propios
-apiRouter.post()//crear evento
-apiRouter.put()//modificar evento
-apiRouter.delete()//eliminar evento
+apiRouter.get("/events", apiController.getAllEvents)//todos los eventos de la bbdd
+apiRouter.get("/myevents", apiController.getMyEvents)//eventos propios
+apiRouter.post("/createvent", apiController.createEvent)//crear evento
+apiRouter.put("/updateevent/:id")//modificar evento
+apiRouter.delete("/deletevent/:id")//eliminar evento
 
 module.exports = apiRouter;
