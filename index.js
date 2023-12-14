0c84879d6a87ada5058ed2383baf3ac811681a7c
Cristina Mateo <cris.mateo.99@gmail.com>
2023-12-14 16:32:43 +0100

    api routes

--- index.js
+++ index.js
@@ -34,12 +34,12 @@ const morgan = require('./middlewares/morgan')
 app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
 
 //rutas
-//const apiroutes = require("./routes/api.routes")
+const apiroutes = require("./routes/api.routes")
 const usersRoutes = require("./routes/users.routes")
 //const authRoutes = require("./routes/oAuth.routes.js")
 
 // //Rutas Template
-//app.use('/api', apiroutes);
+app.use('/api', apiroutes);
 app.use('/',usersRoutes);
 //app.use('/', authRoutes)
 
