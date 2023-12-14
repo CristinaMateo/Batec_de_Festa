349db2e400ba0907e22d0a467a8c60d6391bd380
Cristina Mateo <cris.mateo.99@gmail.com>
2023-12-14 14:24:20 +0100

    corrections to deployment

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Nav from "./Nav";
-import logo from '../../assets/logo.png'
 
 const Header = () => {
   return (
   <header>
     <Nav/>
-    <img id="logo" src={logo} alt="Logo" />
+   
   </header>);
 };
 
--- index.js
+++ index.js
@@ -49,9 +49,9 @@ app.use('*',error404)
 //* Serve static assets in production, must be at this location of this file
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('client/dist'));
     
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'dist','index.html')));
   }
 
 
